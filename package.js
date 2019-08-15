458c763825c1abc60dce51272e76be0a0d3a2131
Nguyen Van Nam <namnv@younetco.com>
2019-08-15 11:56:07 +0700

    update

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
-	name: 'phpfox:phpfox-integration',
+	name: 'phpfox:connect',
 	version: '0.0.1',
-	// Brief, one-line summary of the package.
-	summary: 'rocketchat-phpfox-integration',
-	// URL to the Git repository containing the source code for this package.
-	git: 'https://github.com/phpfox-mobile/rocketchat-integration.git',
-	// By default, Meteor will default to using README.md for documentation.
-	// To avoid submitting documentation, set this field to null.
+	summary: 'phpFox Integration',
+	git: 'https://git.younetco.com/phpfox-mobile/phpfox-integration',
 	documentation: 'README.md',
 });
 
@@ -15,14 +11,20 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
+
 	api.versionsFrom('1.8.0.2');
-	api.use('ecmascript');
-	api.mainModule('phpfox-integration.js');
+
+	api.use([
+		'ecmascript',
+	]);
+
+	api.addFiles('server.js', 'server');
+	api.export('APN', 'server');
 });
 
 Package.onTest(function(api) {
 	api.use('ecmascript');
 	api.use('tinytest');
-	api.use('phpfox:phpfox-integration');
-	api.mainModule('phpfox-integration-tests.js');
+	api.use('phpfox:connect');
+	api.mainModule('phpfox-integration-tests.js', 'server');
 });
